a96142ed43ae5a4b4e9590e099db14ca45012a2f
alimjz <ali.mjz93@gmail.com>
2022-11-19 00:37:44 +0330

    OAuth2Application module added. Users entity moved to MongoDB. campaign entity added to MongoDB.

--- Sales/src/main/java/com/shop/sales/repository/CustomerRepository.java
+++ Sales/src/main/java/com/shop/sales/repository/CustomerRepository.java
@@ -1,8 +1,6 @@
 package com.shop.sales.repository;
 
 import com.shop.core.model.entity.Customer;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
